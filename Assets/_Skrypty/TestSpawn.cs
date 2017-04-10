b680de13320ddc0de124587415ba0d02741efe80
KacperSobolak <kacpersobolak@interia.eu>
2017-04-10 21:12:30 +0200

    Update
    Update game

---
+++ Assets/_Skrypty/TestSpawn.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestSpawn : MonoBehaviour
+{
+
+    public Transform sp;
+    public float st = 1.5f;
+    public GameObject p;
+    public Vector2 size, center;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnPunkty", st, st);
+    }
+
+    void Update()
+    {
+
+    }
+
+    void SpawnPunkty()
+    {
+        Vector2 position = center + new Vector2(Random.Range(-size.x/2, size.x/2), 9);
+        //SpawnTime -= 
+
+        Instantiate(p, position, sp.rotation);
+    }
+}
