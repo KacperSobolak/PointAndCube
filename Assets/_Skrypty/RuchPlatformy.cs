163e5358bf7e9774d05975f2fbe7efac0e4875ed
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2017-04-14 23:33:27 +0200

    Aktualizacja repozytorium

--- Assets/_Skrypty/RuchPlatformy.cs
+++ Assets/_Skrypty/RuchPlatformy.cs
@@ -1,16 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class RuchPlatformy : MonoBehaviour {
 
     public float speed = 10f;   
-    	
+	public float maxSpeed;
+	public float acceleration;
+
+	float move = 0f;
+
 	void FixedUpdate() {
-        float inputspeed = 0f;
+		if (Input.GetKeyDown ("a") || Input.GetKeyDown ("d")) {
+			move = 0;
+		}
 
-        inputspeed = Input.GetAxisRaw("Platforma");
+		if (Input.GetKey ("a")) {
+			move -= acceleration;
+			if (move < -maxSpeed) {
+				move = -maxSpeed;
+			}
+		} 
+		if (Input.GetKey ("d")) {
+			move += acceleration;
+			if (move > maxSpeed) {
+				move = maxSpeed;
+			}
+		}
 
-        transform.position += new Vector3(inputspeed * speed, 0f, 0f);
+		if (Input.GetKeyUp ("a") || Input.GetKeyUp ("d")) {
+			move = 0;
+		}
+
+		transform.position += new Vector3(move, 0f, 0f);
 	}
-
 }
